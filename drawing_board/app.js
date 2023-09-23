8641499fc33db92d529bfc44a0aef66205e2245e
sara <saraact.9@gmail.com>
2023-09-23 20:13:09 +0900

    drawing_board_end

--- drawing_board/app.js
+++ drawing_board/app.js
@@ -1,13 +1,23 @@
+const saveBtn = document.getElementById('save');
+const textInput = document.getElementById('text');
+const fileInput = document.getElementById('file');
+const modeBtn = document.getElementById('mode_btn'); 
+const destroyBtn = document.getElementById('destroy_btn');
+const eraserBtn = document.getElementById('eraser_btn');
 const colorOption = Array.from(document.getElementsByClassName('color-option'));
 const lineWidth = document.getElementById('line_width');
 const color = document.getElementById('color');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
 
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
-isPainting = false;
+ctx.lineCap = 'round';
+let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
     if(isPainting) {
@@ -35,17 +45,78 @@ function onColorCick(event) {
     const colorValue = event.target.dataset.color;
     ctx.strokeStyle = ctx.fillStyle = color.value = colorValue;
 }
+function onModeClick() {
+    if(isFilling){
+        isFilling = false;
+        modeBtn.innerText = 'Fill';
+    } else {
+        isFilling = true;
+        modeBtn.innerText = 'Draw';
+    }
+}
+function onCanvasClick() {
+    if(isFilling){
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
 
+function onDestroyClick() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+    ctx.strokeStyle = 'white';
+    isFilling = false;
+    modeBtn.innerText = 'Fill';
+}
+
+function onFileChange(event) {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src = url;
+    image.onload = function(){
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        fileInput.value = null;
+    }
+}
+
+function onDoubleClick(event){
+    const text = textInput.value;
+    if (text !== "") {
+        ctx.save();
+        ctx.lineWidth = 1;
+        ctx.font = '48px serif'
+        ctx.fillText(text, event.offsetX, event.offsetY);
+        ctx.restore();
+    }
+}
+
+function onSaveClick() {
+    const url = canvas.toDataURL();
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = "myDrwaing.png";
+    a.click();
+}
+
+
+canvas.addEventListener('dblclick', onDoubleClick);
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', onMouseDown);
 canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
-
+canvas.addEventListener('mousedown', onCanvasClick);
 lineWidth.addEventListener('change', onLineWidthChange);
 color.addEventListener('change', onColorChange);
 
 colorOption.forEach(color => color.addEventListener('click',onColorCick));
 
+modeBtn.addEventListener('click', onModeClick);
+destroyBtn.addEventListener('click', onDestroyClick);
+eraserBtn.addEventListener('click', onEraserClick);
+fileInput.addEventListener('change', onFileChange);
+saveBtn.addEventListener('click', onSaveClick);
 // const colors = [
 //     '#55efc4',
 //     '#81ecec',
